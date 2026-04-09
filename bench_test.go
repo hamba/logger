873986a04d233bf83f49032677599bb99560778b
Nicholas Wiersma <nick@wiersma.co.za>
2026-04-09 19:35:50 +0200

    feat: add slog handler (#123)

--- bench_test.go
+++ bench_test.go
@@ -2,6 +2,7 @@ package logger_test
 
 import (
 	"context"
+	"log/slog"
 	"os"
 	"testing"
 
@@ -106,11 +107,26 @@ func BenchmarkLogger_WithContext(b *testing.B) {
 	log := logger.New(discard{}, logger.LogfmtFormat(), logger.Debug)
 	goCtx := context.Background()
 
+	_ = logger.WithContext(goCtx, log, ctx.Str("req_id", "abc123"), ctx.Int("attempt", 1))
+
 	b.ReportAllocs()
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			_ = logger.WithContext(goCtx, log, ctx.Str("req_id", "abc123"), ctx.Int("attempt", 1))
+		}
+	})
+}
+
+func BenchmarkHandler_Logfmt(b *testing.B) {
+	h := logger.NewHandler(discard{}, logger.LogfmtFormat(), slog.LevelDebug)
+	log := slog.New(h)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Error("some message")
 		}
 	})
 }
@@ -124,6 +140,77 @@ func BenchmarkLogger_FromContext(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			log.FromContext(goCtx).Info("some message")
+		}
+	})
+}
+
+func BenchmarkHandler_Json(b *testing.B) {
+	h := logger.NewHandler(discard{}, logger.JSONFormat(), slog.LevelDebug)
+	log := slog.New(h)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Error("some message")
+		}
+	})
+}
+
+func BenchmarkHandler_LogfmtWithGroup(b *testing.B) {
+	h := logger.NewHandler(discard{}, logger.LogfmtFormat(), slog.LevelDebug)
+	log := slog.New(h.WithGroup("service"))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Error("some message")
+		}
+	})
+}
+
+func BenchmarkHandler_JsonWithGroup(b *testing.B) {
+	h := logger.NewHandler(discard{}, logger.JSONFormat(), slog.LevelDebug)
+	log := slog.New(h.WithGroup("service"))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Error("some message")
+		}
+	})
+}
+
+func BenchmarkHandler_LogfmtWithGroupAndAttrs(b *testing.B) {
+	h := logger.NewHandler(discard{}, logger.LogfmtFormat(), slog.LevelDebug)
+	log := slog.New(h.WithGroup("db").WithAttrs([]slog.Attr{
+		slog.String("host", "localhost"),
+		slog.Int("port", 5432),
+	}))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Error("some message", slog.String("driver", "pgx"))
+		}
+	})
+}
+
+func BenchmarkHandler_JsonWithGroupAndAttrs(b *testing.B) {
+	h := logger.NewHandler(discard{}, logger.JSONFormat(), slog.LevelDebug)
+	log := slog.New(h.WithGroup("db").WithAttrs([]slog.Attr{
+		slog.String("host", "localhost"),
+		slog.Int("port", 5432),
+	}))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Error("some message", slog.String("driver", "pgx"))
 		}
 	})
 }
